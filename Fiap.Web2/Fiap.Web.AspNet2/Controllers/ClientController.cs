da5f28738599c0c76a455b330d50ad3efa4ff06f
Mrpitol <33208062+mrpitol@users.noreply.github.com>
2021-08-12 22:25:12 -0300

    Add files via upload

---
+++ Fiap.Web2/Fiap.Web.AspNet2/Controllers/ClientController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Fiap.Web.AspNet2.Models;
+
+namespace Fiap.Web.AspNet2.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("validando o acesso ao controller Home e ação Index");
+            return View();
+        }//Index
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar(ClientModel clientModel)
+        {
+            TempData["mensagemSucesso"]=$"Cliente { clientModel.Nome} cadastrado com sucesso";
+
+            return RedirectToAction ("Index");
+        }
+
+
+        //public IActionResult Cadastrar(ClientModel clientModel)
+        //{
+
+        //    //Capturar os dados digitados
+        //    Console.WriteLine(clientModel);
+        //    //Validar os dados
+        //    //Gravar os dados no banco de dados
+        //    Console.WriteLine("Gravando no banco de dados e recuperando o id");
+
+        //    clientModel.Clienteid = 199;
+
+        //    ViewData["Clienteid"] = clientModel.Clienteid;
+        //    ViewBag.NomeCliente = clientModel.Nome;
+        //    ViewBag.Mensagem = "Mensagem de Teste da ViewBag";
+
+        //    ViewData["ClienteModel"] = clientModel;
+        //    ViewBag.ClientModel = clientModel;
+
+        //    //return RedirecttoAction
+        //    TempData["ClienteModel"] = clientModel;
+
+        //    //Exibir a tela de sucesso
+        //    return View();
+        //}
+
+    }//class
+
+}//namespace
---
+++ Fiap.Web2/Fiap.Web.AspNet2/Models/ClientModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.Web.AspNet2.Models
+{
+    public class ClientModel
+    {
+        public int Clienteid { get; set; }
+        public string Nome { get; set; }
+        public string Email{ get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Observacao { get; set; }
+    }
+}
